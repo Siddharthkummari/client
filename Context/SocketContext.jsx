a04c3d05720090b922752806010e41bccc13fc4c
Yogananda1504 <vynr1504@gmail.com>
2024-07-17 09:10:54 +0530

    chore: Update SocketContext to include additional socket options

--- Context/SocketContext.jsx
+++ Context/SocketContext.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import io from "socket.io-client"; 
-const url =`${import.meta.env.VITE_API_URL}`;
-export const socket = io.connect(url, { 
-    reconnection:true,
-    reconnectionDelay: 500,
-    
-    transports: ['websocket'],
-    
 
+const url = import.meta.env.VITE_API_URL;
+export const socket = io(url, { 
+    reconnection: true,
+    reconnectionDelay: 500,
+    transports: ['websocket','polling'],  // Allow fallback to polling
+    secure: true,  // For WSS connections
+    rejectUnauthorized: false  // Only use this for testing, not in production
 });
+
 export const SocketContext = React.createContext();
